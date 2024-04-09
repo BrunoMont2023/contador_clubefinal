83adffbfbab33070ea958dcd7d50a7103f8f630e
BrunoMont2023 <166160019+BrunoMont2023@users.noreply.github.com>
2024-04-09 22:49:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     enterButton.addEventListener('click', function() {
         // Redirecionar para outra página ao clicar no botão
-        window.location.href = 'outra_pagina.html'; // Substitua 'outra_pagina.html' pela URL desejada
+        window.location.href = 'contadorpage.html'; // Substitua 'outra_pagina.html' pela URL desejada
     });
 });
+// scripts.js (arquivo compartilhado entre páginas)
+
+function iniciarContador() {
+    // Lógica para iniciar o contador geral
+}
+
+function pausarContador() {
+    // Lógica para pausar o contador geral
+}
+
+function resetarContador() {
+    // Lógica para resetar o contador geral
+}
+
+function iniciarContadorGrupo(numeroGrupo) {
+    // Lógica para iniciar o contador do grupo específico
+}
+
+function pausarContadorGrupo(numeroGrupo) {
+    // Lógica para pausar o contador do grupo específico
+}
+
+function resetarContadorGrupo(numeroGrupo) {
+    // Lógica para resetar o contador do grupo específico
+}
+
+// Outras funções compartilhadas entre páginas
